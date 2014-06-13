84c2902365d1e8cd8b54636d50a710ad1e979259
Pascoual <pascoualito@gmail.com>
2014-06-14 00:51:26 +0200

    removed iron-router

--- domLeak.js
+++ domLeak.js
@@ -1,18 +1,14 @@
 DOMLeak = {};
 DOMLeak.blocs = new Meteor.Collection("blocs");
 
-Router.map(function() {
-  this.route('blue', {path: '/', layoutTemplate: 'masterLayout'})
-  this.route('red', {layoutTemplate: 'masterLayout'});
-});
-
 if (Meteor.isClient) {
+  Meteor.startup(function () {Session.set("color", "blue");});
   Template.masterLayout.events({
     'click .blue': function () {
-      Router.go('blue');
+      Session.set("color", "blue");
     },
     'click .red': function () {
-      Router.go('red');
+      Session.set("color", "red");
     }
   });
 
@@ -20,11 +16,14 @@ if (Meteor.isClient) {
     return DOMLeak.blocs.find();
   }
 
-  /*Template.bloc.events({
-    'click .bloc': function () {
-      alert('U Can\'t Touch This: http://en.wikipedia.org/wiki/U_Can\'t_Touch_This');
-    }
-  });*/
+  /*Template.blue.destroyed =
+  Template.red.destroyed = function () {
+    console.log("destroyed");
+  }*/
+
+  Template.masterLayout.isBlue = function () {
+    return Session.equals("color", "blue");
+  }
 }
 
 if (Meteor.isServer) {
